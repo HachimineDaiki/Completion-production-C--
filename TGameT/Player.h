0045bb0fa6791813c7a97dc611402a5f30f521fa
GushikenKaito <kaito-gushiken19h@stu.kbc.ac.jp>
2021-02-20 04:05:07 +0900

    簡易版アニメーションを追加

--- TGameT/Player.h
+++ TGameT/Player.h
@@ -6,12 +6,13 @@ struct Player{
 	int px=500;  //プレイヤーのx座標  ///数字は仮
 	int py=900;  //プレイヤーのy座標
 
-
-	int PlayerNum;   //プレイヤーの画像を変えるための変数
+	int Case=0;   //数字を変えることによってPlayerNum操作する
+	int PlayerNum=0;   //プレイヤーの画像を変えるための変数
 
 }; extern Player player;
 
 void PlayerDisp();  //プレイヤーの描画
 void PlayerMove();   //プレイヤーの動き
+void PlayerAnime();   //プレイヤーのアニメーション
 
 void PlayerAll();   //プレイヤに関する情報をすべてまとめる
