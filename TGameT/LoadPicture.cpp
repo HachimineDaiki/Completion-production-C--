11c7c3514d83cfb1f08ff8454dd7b4210cd2251a
GushikenKaito <kaito-gushiken19h@stu.kbc.ac.jp>
2021-02-22 01:59:34 +0900

    修正

--- TGameT/LoadPicture.cpp
+++ TGameT/LoadPicture.cpp
@@ -10,7 +10,7 @@ int LoadPicture() {
 	if (LoadDivGraph("images/Player.png", 6, 3, 2, 64, 64, Pic.Player) == -1)return -1;  //ƒvƒŒƒCƒ„[‚Ì‰æ‘œ
 	if ((Pic.Enemy = LoadGraph("images/EnemyOct.png")) == -1)return -1; //“G‰æ‘œ“Ç‚İ‚İ
 
-	if ((Pic.playerBullet = LoadGraph("images/shot01.png")) == -1)return -1;   //”wŒi‰æ‘œ‚ğ“Ç‚İ‚İ
+	//if ((Pic.playerBullet = LoadGraph("images/shot01.png")) == -1)return -1;   //¡‚Í‚È‚µ
 
 
 	return 0;
