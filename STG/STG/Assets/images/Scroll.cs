bb93bbbf9fc9bbc9bba79bbc3a0b6969271b26cd
HachimineDaiki <daiki-hachimine-19h@kbc.ac.jp>
2021-02-24 13:52:23 +0900

    Unityシューティング

---
+++ STG/STG/Assets/images/Scroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    //スクロールスピード
+    [SerializeField] float speed = 1;
+
+    private void Update()
+    {
+        //下にスクロールする
+        transform.position -= new Vector3(0, Time.deltaTime * speed);
+
+        if (transform.position.y <= -11f)
+        {
+            transform.position = new Vector2(0, 21.33f);
+        }
+
+    }
+}
