a9debebda4539d4aba605da6306758739c3a93ff
HachimineDaiki <daiki-hachimine-19h@kbc.ac.jp>
2021-02-24 17:54:13 +0900

    2

---
+++ STG/Assets/Scrool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrool : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+
+    private void Update()
+    {
+        transform.position -= new Vector3(0, Time.deltaTime * speed);
+
+        if(transform.position.y <= -11f)
+        {
+            transform.position = new Vector2(0, 21.33f);
+        }
+    }
+}
